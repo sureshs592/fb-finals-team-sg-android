cdaa8a711a49eb180b82a3753362a002b156ca8b
Suresh Subramaniam <suresh@theteamie.com>
2014-11-13 15:47:50 -0800

    Made bubble show up on the screen

--- src/com/fb/finals/team/sg/UIService.java
+++ src/com/fb/finals/team/sg/UIService.java
@@ -2,13 +2,42 @@ package com.fb.finals.team.sg;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 
 public class UIService extends Service {
     
+    private WindowManager windowManager;
+    private View bubble;
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+    }
+    
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        //TODO need to write code to create the UI
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        bubble = inflater.inflate(R.layout.bubble, null);
+        
+        WindowManager.LayoutParams lp = new LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        lp.gravity = Gravity.TOP | Gravity.LEFT;
+        lp.x = 0;
+        lp.y = 100;
+        
+        windowManager.addView(bubble, lp);
         
         return START_NOT_STICKY;
     }
