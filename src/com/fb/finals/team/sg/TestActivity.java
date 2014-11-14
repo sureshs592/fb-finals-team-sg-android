7934a4cb807cd040254eba4ad8c634da1a7bb0d7
Suresh Subramaniam <suresh@theteamie.com>
2014-11-13 18:23:36 -0800

    GCM registration code

--- src/com/fb/finals/team/sg/TestActivity.java
+++ src/com/fb/finals/team/sg/TestActivity.java
@@ -2,13 +2,22 @@ package com.fb.finals.team.sg;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+
 public class TestActivity extends Activity {
+    
+    private GoogleCloudMessaging gcm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        registerDeviceForGCM();
         
         //Prepare a dummy payload and trigger the service.
         //This will eventually by replaced. The service will be triggered through GCM.
@@ -17,6 +26,34 @@ public class TestActivity extends Activity {
         Intent serviceToStart = new Intent(this, UIService.class);
         serviceToStart.putExtra("text", testText);
         startService(serviceToStart);
+    }
+    
+    private void registerDeviceForGCM() {
+        gcm = GoogleCloudMessaging.getInstance(this);
+        new RegisterGCM().execute();
+    }
+    
+    private class RegisterGCM extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            String msg = "";
+            try {
+                String regid = gcm.register("296670291727");
+                msg = "Device registered, registration ID=" + regid;
+                
+            } catch (IOException ex) {
+                msg = "Error :" + ex.getMessage();
+                // If there is an error, don't just keep trying to register.
+                // Require the user to click a button again, or perform
+                // exponential back-off.
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg) {
+            Log.v("GCM", msg);
+        }
     }
 
 }
